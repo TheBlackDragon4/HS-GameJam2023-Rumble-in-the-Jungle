44472f7ca81621867940669230ced1d05d8f31bf
Alexander Ultsch <83983652+TheBlackDragon4@users.noreply.github.com>
2023-06-11 16:16:24 +0200

    Fix and implement the exit of the game

--- scripts/escape-enter-entf.gd
+++ scripts/escape-enter-entf.gd
@@ -9,16 +9,4 @@ func _process(delta):
 	# option to exit the game
 	if Input.is_action_pressed("key_exit"):
 		get_tree().quit()
-	if Input.is_action_pressed("key_pause"):
-		if isPaused == false:
-			_pause_game()
 
-# will be used to change the scene. So we will se a grey screen
-func _pause_game():
-	isPaused = true
-	get_tree().paused = true
-	get_tree().change_scene_to_file("res://PauseScene.tscn")	
-
-#func _resume_game():
-#	isPaused = false
-#	get_tree().paused = false
