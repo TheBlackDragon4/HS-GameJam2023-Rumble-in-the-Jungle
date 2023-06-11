8af8d8732587f0294c8160f4e7c4d64790b871f5
Funzyy <103183666+Funzyy@users.noreply.github.com>
2023-06-11 12:15:50 +0200

    fixed enemy spawn behaviour

--- scripts/Enemy.gd
+++ scripts/Enemy.gd
@@ -3,7 +3,6 @@ extends Area2D
 @export var speed = 300.0
 @export var jump_speed = -400.0
 @export var hp = 10
-signal enemyDead();
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -21,6 +20,10 @@ func _on_hurtbox_hurt(damage):
 	hp -= damage
 	print("took damage")
 	if hp <= 0:
-		queue_free()
 		print("dead")
-		emit_signal("enemyDead")
+		queue_free()
+		# spawn new enemy
+		var enemy = duplicate()
+		enemy.position = Vector2(randf_range(100, 900), -50)
+		print("spawn")
+		get_parent().add_child(enemy)
--- scripts/World.gd
+++ scripts/World.gd
@@ -4,7 +4,14 @@ extends Node2D
 var enemy_scene = preload("res://Enemies/enemy.tscn")
 
 # 1280 x 1024
-func _on_enemy_enemy_dead():
-	var enemy = enemy_scene.instantiate()
-	enemy.position = Vector2(randf_range(100, 1000), -20)
-	add_child(enemy)
+#func _on_enemy_enemy_dead():
+#	# waits till queue is done
+#	print("received signal")
+#	call_deferred("_spawn_enemy")
+#
+#
+#func _spawn_enemy():
+#	var enemy = enemy_scene.instantiate()
+#	enemy.position = Vector2(randf_range(100, 900), -20)
+#	print("spawn")
+#	add_child(enemy)
