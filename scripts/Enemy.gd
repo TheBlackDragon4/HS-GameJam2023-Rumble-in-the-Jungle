28cdda1fa44a4a0a2a7d191867b8036daff036c5
Timperator2 <132988469+Timperator2@users.noreply.github.com>
2023-06-11 15:12:45 +0200

    added snake

--- scripts/Enemy.gd
+++ scripts/Enemy.gd
@@ -6,13 +6,25 @@ extends CharacterBody2D
 # Get the gravity from the project settings so you can sync with rigid body nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+var animation
+
+func _smart_play(name,force=false):
+	if not animation.is_playing() or force:
+		animation.play(name)
+
+
+var timer
+var locked = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	animation = get_node("EnemyAnimation")
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	_smart_play("Idle")
 	pass
 
 func _physics_process(delta):
@@ -33,3 +45,13 @@ func _on_hurtbox_hurt(damage):
 		enemy.position = Vector2(randf_range(400, -550), -500)
 		print("spawn")
 		get_parent().add_child(enemy)
+
+
+func _init():
+	timer = Timer.new()
+	timer.wait_time = 0.75
+	add_child(timer)
+	timer.timeout.connect(_timeout)
+
+func _timeout():
+	locked = 0
